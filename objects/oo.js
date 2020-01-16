c1f74b25d27d1b138b25aa1810faeddd5627c71f
amartins25 <alexandremartins13rj@gmail.com>
2020-01-16 14:52:12 -0300

    course function

---
+++ objects/oo.js
@@ -0,0 +1,19 @@
+// Código não executável!
+
+//Procedural
+processamento(valor1, valor2, valor3)
+
+// Orientação a Objeto => OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        //...
+    }
+}
+
+objeto.processamento() // Foco passou a ser o objeto
+
+//Princípios importantes:
+// 1. Abstração 
