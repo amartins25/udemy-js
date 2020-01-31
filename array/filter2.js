07f36e6a42f0c5b2345ebb81a97ffb368deea6bf
amartins25 <alexandremartins13rj@gmail.com>
2020-01-31 10:23:12 -0300

    reduce

--- array/filter2.js
+++ array/filter2.js
@@ -1,4 +1,13 @@
-// Filtrar um array para ter um subarray
+Array.prototype.filter2 = function(callback) {
+    const newArray = []
+    for (let i = 0; i < this.length; i++) {
+        if(callback(this[i], i, this)) {
+            newArray.push(this[i])
+        }
+    }
+    return newArray
+}
+
 const produtos = [
     { nome: 'Notebook', preco: 2499, fragil: true },
     { nome: 'Ipad Pro', preco: 4199, fragil: true },
@@ -6,12 +15,7 @@ const produtos = [
     { nome: 'Copo de plástico', preco: 18.99, fragil: false }
 ]
 
-console.log(produtos.filter(function(p) {
-    return 
-}))
-
-
 const caro = produto => produto.preco >= 500
 const fragil = produto => produto.fragil
 
-console.log(produtos.filter(caro).filter(fragil))
+console.log(produtos.filter2(caro).filter2(fragil))
