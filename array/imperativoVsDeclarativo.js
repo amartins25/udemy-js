f31606162f83b37af8873b720d4656cece6cf3ab
amartins25 <alexandremartins13rj@gmail.com>
2020-02-03 10:57:22 -0300

    node

--- array/imperativoVsDeclarativo.js
+++ array/imperativoVsDeclarativo.js
@@ -15,6 +15,7 @@ console.log(total1 / alunos.length)
 // Declarativo
 const getNota = aluno => aluno.nota
 const soma = (total, atual) => total + atual
+
 const total2 = alunos.map(getNota).reduce(soma)
 console.log(total2 / alunos.length)
 
