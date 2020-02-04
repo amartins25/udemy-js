aebd2d2d8ba7526037dde8fbcd625bf9df68aacb
amartins25 <alexandremartins13rj@gmail.com>
2020-02-04 11:22:21 -0300

    exports

--- node/pastaA/pastaB/acessarModulo.js
+++ node/pastaA/pastaB/acessarModulo.js
@@ -1,2 +1,13 @@
 const moduloA = require('../../moduloA')
 console.log(moduloA.ola)
+
+const saudacao = require('saudacao')
+console.log(saudacao.ola)
+
+// require é o método de importat
+
+// const http = require('http')
+// http.createServer((req, res) => {
+//     res.write('Bom dia!')
+//     res.end()
+// }).listen(8080)
