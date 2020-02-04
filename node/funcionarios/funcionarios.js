76c0a5181f2c0847634128d9eef55301e8ee24c1
amartins25 <alexandremartins13rj@gmail.com>
2020-02-04 12:05:02 -0300

    using axios

--- node/funcionarios/funcionarios.js
+++ node/funcionarios/funcionarios.js
@@ -5,3 +5,11 @@ axios.get(url).then(response => {
     const funcionarios = response.data
     console.log(funcionarios)
 })
+
+
+
+
+// const caro = produto => produto.preco >= 500
+// const fragil = produto => produto.fragil
+
+// console.log(produtos.filter(caro).filter(fragil))
